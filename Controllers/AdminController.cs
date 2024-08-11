0bdfe018963c7fc81d8733ab6a6e77b8a6d89aa6
DennysRivera <00026919@uca.edu.sv>
2024-08-11 17:37:15 -0600

    Add: Admin controller and views

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using salesAdmin.Models;
+using salesAdmin.Repository.Interfaces;
+
+namespace salesAdmin.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly IProductRepository productRepository;
+    private readonly IUserRepository userRepository;
+
+    public AdminController(IProductRepository productRepository, IUserRepository userRepository)
+    {
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
+    public IActionResult AddProduct()
+    {
+        
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddProduct(Product product){
+        if(!ModelState.IsValid){
+            return View(product);
+        }
+        await productRepository.CreateProduct(product);
+        return RedirectToAction("AddProduct");
+    }
+
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(User user){
+        if(!ModelState.IsValid){
+            return View(user);
+        }
+        await userRepository.CreateUser(user);
+        return RedirectToAction("CreateUser");
+    }
+}
