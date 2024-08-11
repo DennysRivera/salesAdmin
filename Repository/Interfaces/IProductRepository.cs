6f16158069846ef6f9f2a45c22814c59358c1000
DennysRivera <00026919@uca.edu.sv>
2024-08-10 19:52:17 -0600

    Add: Repositories interfaces

---
+++ Repository/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using salesAdmin.Models;
+
+namespace salesAdmin.Repository.Interfaces;
+
+public interface IProductRepository
+{
+    Task CreateProduct(Product product);
+    Task<List<Product>> GetProducts();
+    Task<bool> ProductExists(int id);
+    Task UpdateProduct(Product product);
+    Task DeleteProduct(int id);
+}
