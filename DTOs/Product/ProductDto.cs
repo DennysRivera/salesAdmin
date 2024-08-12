bad69198bde30d5b8c1fa83403e269795af44084
DennysRivera <00026919@uca.edu.sv>
2024-08-12 10:06:34 -0600

    Add: Styles and remove product

--- DTOs/Product/ProductDto.cs
+++ DTOs/Product/ProductDto.cs
@@ -2,6 +2,7 @@
 
 public class ProductDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = "";
     public double UnitPrice { get; set; }
     public int Quantity { get; set; }
